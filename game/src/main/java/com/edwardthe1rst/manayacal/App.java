623f9e1152588da6f7d1c324bad3a9d158b22d75
Bregalad2 <87580945+Bregalad2@users.noreply.github.com>
2022-11-14 22:00:13 -0500

    added maven project

---
+++ game/src/main/java/com/edwardthe1rst/manayacal/App.java
@@ -0,0 +1,13 @@
+package com.edwardthe1rst.manayacal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
